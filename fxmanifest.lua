f5c911e83989389c7543c65590623c620fced20b
EP <ep.kouhia@gmail.com>
2023-08-05 22:32:17 +0300

    Init

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version   'cerulean'
+game         'gta5'
+
+description 'Custom ytyps for enforcing mlo related props from basegame'
+author 'https://github.com/EpKouhia'
+
+files {
+    'stream/basegame/housing_props_01.ytyp',
+    'stream/basegame/housing_props_02.ytyp',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/housing_props_01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/housing_props_02.ytyp'
+
+this_is_a_map 'yes'
